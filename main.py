ee7ed1d2b94db4a5aa32ab89c6240fea9df7133b
pepega <drelkasteam@gmail.com>
2024-12-26 18:48:07 +0700

    Вторая итерация (II)

--- main.py
+++ main.py
@@ -1,13 +1,44 @@
 class Menu:
 
     def input_data(self):
-        pass
+        self.text = None
+        self.text = input("Введите текст: ")
 
     def analyze(self, text):
-        pass
+        text = ''.join(filter(str.isalnum, text))
+        frequency = {}
+        total_chars = len(text)
+        for char in text:
+            if char in frequency:
+                frequency[char] += 1
+            else:
+                frequency[char] = 1
+        self.frequency = {char: count / total_chars for char, count in frequency.items()}
 
-    def print_result(self, frequency):
+    def display_menu(self):
         pass
+    
+    def print_result(self, frequency):
+        print("Частотный анализ текста:")
+        for char, freq in frequency.items():
+            print(f"{char}: {freq:.4f}")
 
     def handle_choice(self, choice):
-        pass
+        if choice == '1':
+            self.input_data()
+        elif choice == '2':
+            if self.text is not None:
+                self.analyze(self.text)
+            else:
+                print("Сначала введите данные.")
+        elif choice == '3':
+            if self.frequency is not None:
+                self.print_result(self.frequency)
+            else:
+                print("Сначала выполните алгоритм.")
+        elif choice == '4':
+            print("Завершение работы программы.")
+            return False
+        else:
+            print("Неверный выбор. Попробуйте снова.")
+        return True
