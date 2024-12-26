dee552315b1c1fc74dc0dcfb5b97e8c939244522
pepega <drelkasteam@gmail.com>
2024-12-26 18:47:02 +0700

    Первая итерация (I)

---
+++ main.py
@@ -0,0 +1,13 @@
+class Menu:
+
+    def input_data(self):
+        pass
+
+    def analyze(self, text):
+        pass
+
+    def print_result(self, frequency):
+        pass
+
+    def handle_choice(self, choice):
+        pass
